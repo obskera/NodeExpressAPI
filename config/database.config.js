87bb35cd45bb673668bdab2b113c6140fcd9b8ab
obskera <vivifydecepi@gmail.com>
2022-06-06 01:59:06 -0700

    changes

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/beetle'
+   // url: 'mongodb://localhost:27017/beetle'
+url: "mongodb+srv://beetle:BeetleBorgs@cluster0.kr3vz.mongodb.net/?retryWrites=true&w=majority"
 }
